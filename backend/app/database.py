397471f07ad058421f53ea2c7cc5d54338a9329a
Farid-SolaradAi <farid.ahmed@solarad.ai>
2024-10-17 19:32:42 +0530

    add db data retrieval function

--- backend/app/database.py
+++ backend/app/database.py
@@ -10,3 +10,13 @@ client = MongoClient(MONGO_URI)
 db = client['rule_engine_db']
 rules_collection = db['rules']
 
+def insert_rule(rule_data):
+    result = rules_collection.insert_one(rule_data)
+    return result.inserted_id
+
+def get_rule(rule_id):
+    return rules_collection.find_one({'_id': ObjectId(rule_id)})
+
+def get_rules(rule_ids):
+    object_ids = [ObjectId(rid) for rid in rule_ids]
+    return list(rules_collection.find({'_id': {'$in': object_ids}}))
